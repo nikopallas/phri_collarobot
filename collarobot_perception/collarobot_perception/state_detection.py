bf054a8dda4a35b495f2aba8cef4bc97ce5f1978
ringer <ringer@cip.ifi.lmu.de>
2026-02-24 15:23:48 +0100

    state_detection

--- collarobot_perception/collarobot_perception/state_detection.py
+++ collarobot_perception/collarobot_perception/state_detection.py
@@ -1,20 +1,74 @@
-from typing import Tuple, List
-from .take_image import capture_frame
-from .detect_zones import get_state as analyze_zones_state
+
+from take_image import capture_frame
+from detect_zones import get_state as analyze_zones_state
+import cv2
+from pathlib import Path
+import rclpy
+from rclpy.node import Node
+import json
+
+from std_msgs.msg import String
+
+IMAGE_DIR = Path(__file__).parent.parent / "images"
+TEST_IMAGE_PATH = IMAGE_DIR / "captured_test.png"
 
 
-def get_state(debug=False) -> Tuple[List, List, List]:
+class StatePublisher(Node):
+
+    def __init__(self):
+        super().__init__('state_publisher')
+        self.publisher_ = self.create_publisher(String, 'state', 10)
+        timer_period = 0.5
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+
+    def timer_callback(self):
+        msg = String()
+
+        msg.data = json.dumps(get_state(), indent=4)
+        self.publisher_.publish(msg)
+        self.get_logger().info('Publishing: "%s"' % msg.data)
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    minimal_publisher = StatePublisher()
+
+    rclpy.spin(minimal_publisher)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    minimal_publisher.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
+
+
+def get_state(debug=False) -> dict:
     """
     Captures a frame from the webcam and returns the current state of markers in zones.
     Returns:
-        Tuple[storage, proposed, accepted]: Lists of marker IDs in each zone.
+        dict: Dictionary containing the state of markers in zones.
     """
     frame = capture_frame()
-    if frame is None:
-        print("Error: Could not capture frame from webcam.")
-        return [], [], []
+    # if frame is None:
+    #     print("Error: Could not capture frame from webcam.")
+    #     return [], [], []
 
-    return analyze_zones_state(frame, debug=debug)
+    frame = cv2.imread(str(TEST_IMAGE_PATH))
+
+    storage, proposed, accepted = analyze_zones_state(frame, debug=debug)
+
+    state = {
+        "storage": storage,
+        "proposed": proposed,
+        "accepted": accepted
+    }
+
+    return state
 
 
 if __name__ == "__main__":
--- collarobot_perception/setup.py
+++ collarobot_perception/setup.py
@@ -24,6 +24,7 @@ setup(
     },
     entry_points={
         'console_scripts': [
+            'state_publisher = collarobot_perception.state_detection:main',
         ],
     },
 )
